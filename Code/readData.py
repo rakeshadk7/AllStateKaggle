435c8415c6b488c35d6c99fd35bf76adc45292e4
Rakesh <rakesh.adk7@gmail.com>
2016-11-03 10:56:17 -0500

    Modified Code

--- Code/readData.py
+++ Code/readData.py
@@ -7,12 +7,28 @@ test = pd.read_csv("..\\Data\\test.csv")   #read test dataset
 
 #print train.describe()
 
-train = train.drop(['id'], axis=1) #id is not a feature, so drop it
-test = test.drop(['id'], axis=1)
+x = train.drop(['loss','id'], 1)
+y = np.log1p(train["loss"]) #Apply log 1 + loss. Note: remeber to subtract shift when getting end result
+ids = test['id'] #needed for writing results.csv
 
-train["loss"] = np.log1p(train["loss"]) #Aplly log 1 + loss
+#Now, we need to split the dataset's columns into categorical features and continuous features and hot encode all categorical features
 
-#Now, we need to split the dataset's columns into categorical features and continuous features and use hot encode all categorical features
+cat = []
+cont = []
+for colName in x.columns:
+    if colName.startswith("cat"):
+        cat.append(colName)
+    elif colName.startswith("cont"):
+        cont.append(colName)
+    else:
+        print "Oops! Unknown ColName encountered"
+        
+        
+
+
+
+
+
 
 
 
