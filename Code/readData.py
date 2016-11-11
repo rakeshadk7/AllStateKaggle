86667574990469e18f999e784b087664baebc5ba
Sureya Sathiamoorthi <catchsureya@gmail.com>
2016-11-11 11:50:58 +0000

    Update readData.py

--- Code/readData.py
+++ Code/readData.py
@@ -1,44 +1,25 @@
-import sklearn
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
-
-train = pd.read_csv("..\\Data\\train.csv") #read train dataset
-test = pd.read_csv("..\\Data\\test.csv")   #read test dataset
-
-#print train.describe()
-
-x = train.drop(['loss','id'], 1)
-y = np.log1p(train["loss"]) #Apply log 1 + loss. Note: remeber to subtract shift when getting end result
-ids = test['id'] #needed for writing results.csv
-
-'''
-Split the features into continuous and categorical,
-Simpler way to do this is to just read first 116 columns as categorical and the remaining as continious.
-But I hate hardcoding, so .... 
-'''
-cat = []
-cont = []
-for colName in x.columns:
-    if colName.startswith("cat"):
-        cat.append(colName)
-    elif colName.startswith("cont"):
-        cont.append(colName)
-    else:
-        print "Oops! Unknown ColName encountered"
-
-'''    
-le = LabelEncoder() 
-oHe = OneHotEncoder()
-x[cat] = x[cat].apply(le.fit_transform, axis = 1) #Apply encoder to all columns with categorical features 
-
-'''
 
 
 
+train = pd.read_csv("train.csv")
+_columns = list(train)
+
+_cat = [x for x in _columns if x.startswith("cat")]
+_con = [x for x in _columns if x.startswith("con")]
 
 
+# This loops tries to encode the values for CAT variables.
+#Takes too long.can you please try and make it efficient when have time .
 
+for cat_column_name in _cat:
+
+        uniq_col_values = list(set(train[cat_column_name]))
+
+        for uniq_value in uniq_col_values:
+                train.loc[train[cat_column_name] == uniq_value, "{}_{}".format(
+                    cat_column_name, uniq_value)] = 1
+                train.loc[train[cat_column_name] != uniq_value, "{}_{}".format(
+                    cat_column_name, uniq_value)] = 0
 
 
